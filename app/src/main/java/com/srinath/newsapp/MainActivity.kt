9d0426a5ab9a1b7bf21c88d559371dec79056552
hellosrinath <s.nath32@yahoo.com>
2023-10-04 12:03:55 +0600

    adding bottom navigation

--- app/src/main/java/com/srinath/newsapp/MainActivity.kt
+++ app/src/main/java/com/srinath/newsapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.srinath.newsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.srinath.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        // connecting bottom navigation to fragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(
+            navController
+        )
+
     }
 }
